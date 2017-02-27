4d66aecbd220b7679835a1758020e2861670845b
Nathaniel Meyer and Sara Schultz <sara@>
2017-02-27 12:06:12 -0800

    Add list maker; delete items by clicking notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
 // Business logic
-var echoFunction = function(string) {
-  return string;
-};
+function ListItem(taskName, taskNotes) {
+  this.taskName = taskName;
+  this.taskNotes = taskNotes;
+}
 
 // Front end logic
 $(function() {
   $("form#inputForm").submit(function(event){
     event.preventDefault();
-    userString = $("input#userInput").val();
-    if (!userString) {
-      $("#errorFeedback").show();
-    } else {
-      $("#errorFeedback").hide();
-      output = echoFunction(userString);
-      $(".response").show();
-      $("#echoInput").text(output);
-    }
-    echoInput = "";
+
+    var inputtedTaskName = $("input#inputItem").val();
+    var inputtedTaskNotes = $("input#inputNotes").val();
+
+    var newListItem = new ListItem(inputtedTaskName, inputtedTaskNotes);
+
+    $("ul#listItems").append("<li><span class='listItem'>" + newListItem.taskName + "</span></li>");
+
+    $("input#inputItem").val("");
+    $("input#inputNotes").val("");
+
+    $(".listItem").last().click(function() {
+      $(".foo").removeClass("foo");
+      $(this).addClass("foo");
+
+      $("#show-listItem").show();
+      $("#show-listItem h2").text(newListItem.taskName);
+      $(".inputItem").text(newListItem.taskName);
+      $(".inputNotes").text(newListItem.taskNotes);
+    });
+
+    $(".inputNotes").click(function() {
+      $(".foo").remove();
+    });
   });
 });
